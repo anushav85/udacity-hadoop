1da6aba2e8339443098fa2a34199efdc6e6ba525
Juan <jabaldonedo@gmail.com>
2014-03-10 18:53:37 +0100

    3.1 files made executables

--- lesson3/3.1/reducer.py
+++ lesson3/3.1/reducer.py
@@ -1,22 +1,24 @@
+#!/usr/bin/python
+
 import sys
 
 saltesTotal = 0
 oldKey = None
 
 for line in sys.stdin:
-	data_mapped = line.strip().split("\t")
-	if len(data_mapped) != 2:
-		continue
+    data_mapped = line.strip().split("\t")
+    if len(data_mapped) != 2:
+        continue
 
-	thisKey, thisSale = data_mapped
+    thisKey, thisSale = data_mapped
 
-	if oldKey and oldKey != thisKey:
-		print("{0}\t{1}".format(oldKey, saltesTotal))
-		oldKey = thisKey
-		saltesTotal = 0
+    if oldKey and oldKey != thisKey:
+        print("{0}\t{1}".format(oldKey, saltesTotal))
+        oldKey = thisKey
+        saltesTotal = 0
 
-	oldKey = thisKey
-	saltesTotal += float(thisSale)
+    oldKey = thisKey
+    saltesTotal += float(thisSale)
 
 if oldKey != None:
-	print("{0}\t{1}".format(oldKey, saltesTotal))
+    print("{0}\t{1}".format(oldKey, saltesTotal))
