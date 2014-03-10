e4466a312aad8d5f1f8f5df4c7da2f406a781955
Juan <jabaldonedo@gmail.com>
2014-03-10 19:04:12 +0100

    adding mapper/reducer for 3.2

---
+++ lesson3/3.2/reducer.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+import sys
+
+sale_highest = 0
+old_key = None
+
+for line in sys.stdin:
+    data_mapped = line.strip().split("\t")
+    if len(data_mapped) != 2:
+        continue
+
+    this_key, this_sale = data_mapped
+
+    if old_key and old_key != this_key:
+        print("{0}\t{1}".format(old_key, sale_highest))
+        old_key = this_key
+        sale_highest = 0
+
+    old_key = this_key
+
+    if this_sale > sale_highest:
+        sale_highest = this_sale
+
+if old_key is not None:
+    print("{0}\t{1}".format(old_key, sale_highest))
