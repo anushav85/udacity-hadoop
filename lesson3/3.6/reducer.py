6496e8efa827a860cadca23cd608191119b138cf
Juan <jabaldonedo@gmail.com>
2014-03-11 13:25:55 +0100

    reducer did not take into account last record

--- lesson3/3.6/reducer.py
+++ lesson3/3.6/reducer.py
@@ -17,4 +17,9 @@ for line in sys.stdin:
     count_hits += 1
     old_file = this_file
 
+if old_file is not None:
+    if count_hits > popular_hits:
+        popular_file = this_file
+        popular_hits = count_hits
+
 print("{0} {1}".format(popular_file, popular_hits))
