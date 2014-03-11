21b899a42021dbdce9a1dc96a896ca64c51bc0d1
Juan <jabaldonedo@gmail.com>
2014-03-11 11:39:04 +0100

    PEP8 compliant

--- lesson3/3.4/mapper.py
+++ lesson3/3.4/mapper.py
@@ -3,11 +3,10 @@
 import sys
 import re
 
-regex = re.compile("(?P<host>\S+) (?P<identity>\S+) (?P<username>\S+) \[(?P<time>.+)\] \"(?P<method>.+) (?P<request>.+) (?P<protocol>.+)\" (?P<status>[0-9]+) (?P<size>\S+)")
+regex = re.compile("(?P<host>\S+) (?P<identity>\S+) (?P<username>\S+) \[(?P<time>.+)\] \"(?P<method>.+) (?P<request>.+)"
+                   " (?P<protocol>.+)\" (?P<status>[0-9]+) (?P<size>\S+)")
 
- 
 for line in sys.stdin:
     r = regex.search(line)
     res = r.groupdict()
     print(res[u'request'])
-
